acce3acfe6890c77b623a83aebbe6ddf5ce9fb88
Lukasz Wisniewski <wsa3@vp.pl>
2023-04-07 15:05:34 +0100

    adding new improved tasks

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 {
     const tasks = [
         {
-            content: "Nagrać lekcję",
+            contenr: "Nagrać lekcje",
             done: false,
         },
         {
-            content: "Zjeść pierogi",
+            content: "zjeść poerogi",
             done: true,
+
         },
 
     ];
@@ -16,60 +17,47 @@
 
         for (const task of tasks) {
             htmlString += `
-        <li
-         ${task.done ? "  style=\"text-decoration: line-through\"" : ""} 
-         >
-         <button class="js-reove">Usuń</button>
-        ${task.content}
-
-        </li>
-        `;
+            <li 
+            ${task.done ? "style=\"text-decoration:line-through\"" : ""}
+            >
+               ${task.content}
+            </li>
+            `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+        };
 
-        const removeButtons = document.querySelectorAll(".js-remove");
-
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                tasks.splice(index, 1);
-                render();
-            });
-        });
-      
-
-    };
-
-    const addNewTask = (newTaskContent) => {
+    const addNewTask = (newTaskContent) =>{
         tasks.push({
-            content: newTaskContent,
-           
-        });
-
-        render();
-    };
-
-    const onFormSubmit = (event) => {
+            content: newTaskContent, 
+         });
+ 
+         render();
+    };   
+    
+    const onFormSubmit =  (event) => {
         event.preventDefault();
-
+        
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
         
         if (newTaskContent === "") {
             return;
-        }
-
+        };
+        
         addNewTask(newTaskContent);
+        
 
-
-    }
+       }
 
     const init = () => {
         render();
 
-        const form = document.querySelector(".js-form");
+       const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", onFormSubmit);
+       form.addEventListener("submit", onFormSubmit);
     };
 
     init();
+
 }
