e21d2a76e4950120d9333d3df1d88d7946022e33
Lukasz Wisniewski <wsa3@vp.pl>
2023-04-07 15:46:38 +0100

    remove tasks

--- script.js
+++ script.js
@@ -12,6 +12,21 @@
 
     ];
 
+    const addNewTask = (newTaskContent) =>{
+        tasks.push({
+            content: newTaskContent, 
+         });
+ 
+         render();
+
+        }; 
+
+         const removeTask = (taskIndex) =>{
+            tasks.splice(taskIndex, 1);
+            render();
+         }
+     
+
     const render = () => {
         let htmlString = "";
 
@@ -20,21 +35,25 @@
             <li 
             ${task.done ? "style=\"text-decoration:line-through\"" : ""}
             >
+            <button class="js-remove" >USUŃ</button>
                ${task.content}
             </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+        
+        const removeButtons = document.querySelectorAll(".js-remove");
+         
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () =>{
+            removeTask(index);
+            });
+        });
+
         };
 
-    const addNewTask = (newTaskContent) =>{
-        tasks.push({
-            content: newTaskContent, 
-         });
- 
-         render();
-    };   
+    
     
     const onFormSubmit =  (event) => {
         event.preventDefault();
