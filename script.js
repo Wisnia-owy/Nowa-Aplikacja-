32a495d5831985bd4ebb69379afea13a88be6886
Lukasz Wisniewski <wsa3@vp.pl>
2023-04-07 16:05:49 +0100

    binding events moved to seperate function

--- script.js
+++ script.js
@@ -31,6 +31,25 @@
             render();
          }
 
+         const bindEvents = () =>{
+            const removeButtons = document.querySelectorAll(".js-remove");
+         
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () =>{
+            removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+         
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () =>{
+            toggleTaskDone(index);
+            });
+        });
+
+         }
+
     const render = () => {
         let htmlString = "";
 
@@ -47,21 +66,7 @@
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
         
-        const removeButtons = document.querySelectorAll(".js-remove");
-         
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () =>{
-            removeTask(index);
-            });
-        });
-
-        const toggleDoneButtons = document.querySelectorAll(".js-done");
-         
-        toggleDoneButtons.forEach((toggleDoneButton, index) => {
-            toggleDoneButton.addEventListener("click", () =>{
-            toggleTaskDone(index);
-            });
-        });
+        bindEvents();
 
         };
 
